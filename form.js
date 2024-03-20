e7c1c27124d5a60a786b4682032aef7bb3960c04
Rockstarjoyel <79788002+Joyel001@users.noreply.github.com>
2024-03-20 15:17:23 +0530

    Add files via upload

---
+++ form.js
@@ -0,0 +1,41 @@
+// function validate(){
+//     var name = document.getElementById("fname").value;
+//     console.log(name);
+
+//     var email = document.getElementById("email").value;
+//     console.log(email);
+
+//     var password = document.getElementById("password").value;
+//     console.log(password);
+
+//     var textarea = document.getElementById("textarea").value;
+//     console.log(textarea);
+
+//     var gender = document.getElementById("gender").value;
+//     console.log(gender);
+
+//     var cars = document.getElementById("cars").value;
+//     console.log(cars);
+
+//     var color = document.getElementById("color").value;
+//     console.log(color);
+
+//     var select = document.getElementById("select").value;
+//     console.log(select);
+
+//     var select2 = document.getElementById("select2").value;
+//     console.log(select2);
+
+//     var name = document.getElementById("fname").value;
+//     console.log(name);
+
+//     if(name === "" || name.toLowerCase() === "a"){
+//         console.log("Please enter your name");
+//         return false; // Prevent form submission
+//     }
+//     for(name=0;name<100000;name++){
+//         console.log("Please enter your name not number");
+//         return false; // 
+//     }
+//     return true; // Allow form submission
+// }
---
+++ index.js
@@ -0,0 +1,13 @@
+// const cars = ["BMW", "Volvo", "Saab", "Ford", "Fiat", "Audi"];
+
+// let text = "";
+// for (let i = 0; i < cars.length; i++) {
+//   text += cars[i] + "<br>";
+//   console.log(text)
+// }
+
+let text = "";
+
+for (let i = 0; i < 5; i++) {
+  text += "The number is " + i + "<br>";
+}
